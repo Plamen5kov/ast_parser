53475ae0931659b566973f77d0551332b5356255
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-01-22 18:33:02 +0200

    fixed error handling and implemented logging

--- parse.js
+++ parse.js
@@ -1,20 +1,30 @@
-var fs = require("fs"),
-	babelParser = require("babylon");
+loggingSettings = {
+	"logDirectory" : "logs",
+	"strategy" : "console",
+	"APP_NAME" : "ast_parser"
+};
+// loggingSettings.level = 'info';
 
+var fs = require("fs"),
+	babelParser = require("babylon"),
+	logger = require('log-client-node')(loggingSettings);
+
+//TODO: get from environment variable and leave this as default
 var outFile = "out/ast.txt";
 
 var readFile = function (currentFilePath) {
 	return new Promise(function (resolve, reject) {
+
 		fs.readFile(currentFilePath, function (err, data) {
-			if(data) {
-				console.log("\t +got content of file!");
-				resolve(data.toString());
-			}
+
 			if(err) {
-				console.log("\t +DIDN'T get content of file!");
-				console.log(err);
-				reject(false);
+				logger.error("\t+DIDN'T get content of file!");
+				logger.error(err);
+				reject(err);
 			}
+
+			logger.info("\t+got content of file!");
+			resolve(data.toString());			
 		});
 	});
 }
@@ -22,39 +32,38 @@ var readFile = function (currentFilePath) {
 var astFromFileContent = function (data, err) {
 	return new Promise(function (resolve, reject) {
 
-		if(data) {
-			console.log("\t +parsing ast from file!");
-			var ast = babelParser.parse(data);
-			// var ast = babelParser.parse("var = 4"); //try if error handling works ok
-			resolve(ast);
-		}
 		if(err) {
-			console.log("\t +DIDN'T parse ast from file!");
-			//todo: pass some kind of err
-			reject(false);
+			logger.error("\t+DIDN'T parse ast from file!");
+			reject(err);
 		}
+		
+		logger.info("\t+parsing ast from file!");
+		var ast = babelParser.parse(data);
+		// var ast = babelParser.parse("var = 4"); //try if error handling works ok
+		resolve(ast);
 	});
 };
 
 var writeToFile = function(data, err) {
 	return new Promise (function (resolve, reject) {
-	 	fs.writeFile(outFile, JSON.stringify(data), function (fileError) {
+	 	fs.writeFile(outFile, JSON.stringify(data, null, 4), function (writeFileError) {
 
 			if(err) {
-				console.log("\t +DIDN'T write to file!");
-				reject(false);
+				logger.error("Error from writeToFile: " + err);
+				reject(err);
+			}
+			if(writeFileError) {				
+				logger.error("Error writing file: " + writeFileError);
+				reject(writeFileError);
 			}
 
-			if(fileError) {
-				reject(fileError);
-			}
-			console.log("\t +wrote to file: " + outFile);
+			logger.info("\t+wrote to file: " + outFile);
 			resolve(data);
 		});
 	});
 }
 
-//todo: implement and require from another file
+//TODO: implement and require from another file
 var visitorsPackage = {
 	"decoratorVisitor": function decoratorVisitor() {},
 	"extendVisitor": function extendVisitor() {},
@@ -63,26 +72,25 @@ var visitorsPackage = {
 
 var visitAst = function (data, err) {
 	return new Promise (function (resolve, reject) {
-		if(data) {
-			console.log("\t +visiting ast with given visitor library!");
-			visitWith(data, visitorsPackage);
-		}
 		if(err) {
-			console.log("\t +DIDN'T visit ast!");
-			reject(false);
+			logger.error("\t+DIDN'T visit ast!");
+			reject(err);
 		}
+
+		logger.info("\t+visiting ast with given visitor library!");
+		visitWith(data, visitorsPackage);
 	});
 }
 
 var visitWith = function(ast, visitorsForEs5) {
-	console.log("\t +visiting happens here");
+	logger.info("\t+visiting happens here");
 }
 
 var exceptionHandler = function (reason) {
-	console.log("Caught error: " + reason);
+	logger.info("Caught error: " + reason);
 }
 
 readFile("app/test.js").then(astFromFileContent)
-			.then(writeToFile) //fluent api for debugging purposes
-			.then(visitAst)
-			.catch(exceptionHandler)
+						.then(writeToFile) //fluent api for debugging purposes
+						.then(visitAst)
+						.catch(exceptionHandler)
