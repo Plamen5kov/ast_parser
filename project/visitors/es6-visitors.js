b242a6f80a9fef63acf5d5cdc192df95a7fdf676
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-02-05 09:41:28 +0200

    all errors are thrown after visitor passed

--- project/visitors/es6-visitors.js
+++ project/visitors/es6-visitors.js
@@ -4,7 +4,8 @@ var es6_visitors = (function () {
 		defaultExtendDecoratorName = "JavaProxy",
 		isExtendClassDecorator = false,
 		isClassDeclaration = false,
-		isClassDecorator = false,
+		isStringArgument = false,
+		isCorrectClassName = false,
 		overriddenMethodNames = [],
 		extendClass = [],
 		classNameFromDecorator = "No decorator name found",
@@ -16,7 +17,6 @@ var es6_visitors = (function () {
 
 		// get extend class name from decorator
 		if(t.isDecorator(path.node) && t.isClassDeclaration(path.parent)) {
-			isClassDecorator = true;
 			
 			// node is our custom class decorator (must be called with parameters)
 			if(t.isCallExpression(path.node.expression) && path.node.expression.callee.name === extendDecoratorName) {
@@ -28,22 +28,13 @@ var es6_visitors = (function () {
 				isExtendClassDecorator = true;
 
 				var decoratorArguments = path.node.expression.arguments;
-				if(decoratorArguments.length > 1) {
-					throw "There are more than one arguments passed to the '" + extendDecoratorName + "' decorator!"
-				}
-
 				var arg0 = decoratorArguments[0];
-				if (t.isStringLiteral(arg0)) {
+				isStringArgument = t.isStringLiteral(arg0)
+				if (isStringArgument) {
 					classNameFromDecorator = arg0.value;
 				}
-				else {
-					throw "The decorator: '" + extendDecoratorName + "' needs to have a string as a first parameter. Example: '" + extendDecoratorName + "(\"a.b.C\")'"
-				}
 
-				var isCorrectClassName = /^(((\w+\.)+\w+)|(\w+))$/.test(classNameFromDecorator);
-				if(!isCorrectClassName) {
-					throw "The argument '" + classNameFromDecorator + "' of the '" + extendDecoratorName + "' decorator is not following the right pattern which is: '[namespace.]ClassName'. Example: 'a.b.ClassName'";
-				}
+				isCorrectClassName = /^(((\w+\.)+\w+)|(\w+))$/.test(classNameFromDecorator);
 			}
 		}
 
@@ -53,10 +44,6 @@ var es6_visitors = (function () {
 				config.logger.info("\t+in class declaration");
 			}
 			classDeclarationCount++;
-
-			if(classDeclarationCount > 1) {
-				throw "There is more than one class declaration in one file!" //TODO: specify file!
-			}
 
 			isClassDeclaration = true;
 			var o = path.node.superClass;
@@ -88,21 +75,30 @@ var es6_visitors = (function () {
 	function clearDataInner() {
 		isExtendClassDecorator = false;
 		isClassDeclaration = false;
-		isClassDecorator = false;
 		overriddenMethodNames = [];
 		extendClass = [];
 		classNameFromDecorator = "No decorator name found";
-		classDeclarationCount = 0;		
+		classDeclarationCount = 0;	
 	}
 
 	decoratorVisitor.clearData = function() {
 		clearDataInner();
 	}
 	decoratorVisitor.getDecoratorClassName = function () {
+		 //TODO: specify file in exception
 		if(!isExtendClassDecorator) {
 			throw "The node you are trying to parse has no class decorator: '" + extendDecoratorName + "', which is required. Example: '" + extendDecoratorName + "(\"a.b.ClassName\")'";
 		}
-		
+		if(!isStringArgument) {
+			throw "The decorator: '" + extendDecoratorName + "' needs to have a string as a first parameter. Example: '" + extendDecoratorName + "(\"a.b.C\")'"
+		}
+		if(!isCorrectClassName) {
+			throw "The argument '" + classNameFromDecorator + "' of the '" + extendDecoratorName + "' decorator is not following the right pattern which is: '[namespace.]ClassName'. Example: 'a.b.ClassName'";
+		}
+		if(classDeclarationCount > 1) {
+			throw "There is more than one class declaration in one file!"
+		}
+
 		return classNameFromDecorator;
 	}
 	decoratorVisitor.getExtendClass = function () {
