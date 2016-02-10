0a515c246d5c46d429b36425a49512e71a15b44d
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2016-02-10 10:41:10 +0200

    added ts transpiled es5 visitor to es5 visitors

---
+++ project/transpiled_ts_parser.js
@@ -0,0 +1,179 @@
+///////////////// config /////////////////
+
+var disableLogger = false;
+if(process.env.AST_PARSER_DISABLE_LOGGING && process.env.AST_PARSER_DISABLE_LOGGING.trim() === "true") {
+	disableLogger = true;
+}
+
+loggingSettings = {
+	"logDirectory" : "logs",
+	"strategy" : "console",
+	"APP_NAME" : "ast_parser",
+	"disable": disableLogger
+};
+
+var fs = require("fs"),
+	babelParser = require("babylon"),
+	traverse = require("babel-traverse"),
+	logger = require('./helpers/logger')(loggingSettings),
+	path = require("path"),	
+	stringify = require("./helpers/json_extension"),
+	// es6_visitors = require("./visitors/es6-visitors"),
+	es5_visitors = require("./visitors/es5-visitors"),
+	t = require("babel-types"),
+	filewalker = require('filewalker'), 
+
+	appDir = path.dirname(require.main.filename),
+	extendDecoratorName = "JavaProxy", // TODO: think about name
+	outFile = "out/out_parsed_typescript.json", //default out file
+	inputDir = "input_parced_typescript";
+
+if(process.env.AST_PARSER_OUT_FILE) {
+	outFile = process.env.AST_PARSER_OUT_FILE.trim();
+}
+
+if(process.env.AST_PARSER_INPUT_DIR) {
+	inputDir = process.env.AST_PARSER_INPUT_DIR.trim();
+}
+
+/////////////// init ////////////////
+function cleanOutFile(filePath) {
+	fs.truncateSync(filePath, 0);
+	logger.info("+cleared out file: " + filePath);
+}
+
+function createFile(filePath) {
+	if(ensureDirectories(outFile)) {
+		fs.writeFileSync(outFile, "");
+		logger.info("+created ast output file: " + path.join(appDir, outFile));
+	}
+	cleanOutFile(filePath)
+}
+
+function ensureDirectories(filePath) {
+	var parentDir = path.dirname(filePath);
+	if(fs.existsSync(parentDir)) {
+		return true;
+	}
+
+	ensureDirectories(parentDir);
+	fs.mkdirSync(parentDir);
+	return true;
+}
+
+createFile(outFile)
+
+/////////////// execute ////////////////
+
+var traverseFilesDir = function(filesDir) {
+	if(!fs.existsSync(filesDir)) {
+		throw "The input dir: " + filesDir + " does not exist!";
+	}
+
+	filewalker(filesDir)
+		.on("file", function (file, info) {
+			if(file.substring(file.length - 3, file.length) === '.js') {
+				var currentFileName = path.join(filesDir, file);
+				readFile(currentFileName)
+					.then(astFromFileContent)
+					// .then(writeToFile)
+					.then(visitAst)
+					.then(writeToFile)
+					.catch(exceptionHandler)
+			}
+		})
+		.on('error', function(err) {
+			reject(err);
+		})
+		.walk();
+}
+
+traverseFilesDir(inputDir);
+
+var readFile = function (filePath, err) {
+	return new Promise(function (resolve, reject) {
+
+		fs.readFile(filePath, function (err, data) {
+
+			if(err) {
+				logger.warn("+DIDN'T get content of file!");
+				return reject(err);
+			}
+
+			logger.info("+got content of file!");
+			return resolve(data.toString());
+		});
+	});
+}
+
+var astFromFileContent = function (data, err) {
+	return new Promise(function (resolve, reject) {
+
+		if(err) {
+			logger.warn("+DIDN'T parse ast from file!");
+			return reject(err);
+		}
+		
+		logger.info("+parsing ast from file!");
+		var ast = babelParser.parse(data, {
+						plugins: ["decorators"]
+					});
+		return resolve(ast);
+	});
+};
+
+var visitAst = function (data, err) {
+	return new Promise (function (resolve, reject) {
+		if(err) {
+			logger.warn("+DIDN'T visit ast!");
+			return reject(err);
+		}
+
+		logger.info("+visiting ast with given visitor library!");
+
+		traverse.default(data, {
+			enter(path) {
+
+				var decoratorConfig = {
+					logger: logger,
+					extendDecoratorName: extendDecoratorName
+				};
+				es5_visitors.decoratorVisitor(path, decoratorConfig);				
+			}
+		})
+
+		var linesToWrite = es5_visitors.decoratorVisitor.getExtendInfo().join("\n")
+		return resolve(linesToWrite)
+	});
+}
+
+var writeToFile = function(data, err) {
+
+	return new Promise (function (resolve, reject) {
+
+		fs.appendFile(outFile, data, function (writeFileError) {
+			if(err) {
+				logger.warn("Error from writeToFile: " + err);
+				return reject(err);
+			}
+			if(writeFileError) {				
+				logger.warn("Error writing file: " + writeFileError);
+				return reject(writeFileError);
+			}
+
+			logger.info("+appended '" + data + "' to file: " + outFile);
+			return resolve(data);
+		});
+	});
+}
+
+var exceptionHandler = function (reason) {
+	if(reason.errCode && reason.errCode === 1) {
+		logger.error("(*)(*)(*)Error: Exception Handler Caught: " + reason.message);
+		logger.error("PROCESS EXITING...");
+		process.exit(4);
+	}
+	else {
+		logger.error("(*)(*)(*)Error: Exception Handler Caught: " + reason);
+	}
+}
--- project/visitors/es5-visitors.js
+++ project/visitors/es5-visitors.js
@@ -1,103 +1,216 @@
 var es6_visitors = (function () {
 	var t = require("babel-types"),
 
-		defaultExtendDecoratorName = "extendDecorator",
-		isExtendKeyWord = false,
-		overriddenMethodNames = [],
-		extendClass = [],
-		classNameFromDecorator = "No decorator name found";
+		defaultExtendDecoratorName = "JavaProxy",
+		linesToWrite = [];
+
 
 	function decoratorVisitor(path, config) {
 
+		// anchor is extend
 		if (t.isMemberExpression(path) && path.node.property.name === "extend") {
+			traverseNormalExtend(path, config);
+		}
 
-			if(config.logger) {
-				config.logger.info("\t+in extend function");
-			}
-
-			isExtendKeyWord = true;
-			var callee = path.parent.callee;
-			if(!callee) {
-				throw {
-					// TODO: specify file
-					message: "You need to specify a name of the 'extend'. Example: '...extend(\"a.b.C\", {...overrides...})')",
-					errCode: 1
-				}
-			}
-
-			var o = callee.object
-			if(o) {
-				while (true) {
-					if (t.isIdentifier(o)) {
-						extendClass.push(o.name)
-						break;
-					}
-
-					extendClass.push(o.property.name)
-					o = o.object
-				}
-			}
-
-			var arg0 = path.parent.arguments[0];
-			if (t.isStringLiteral(arg0)) {
-				classNameFromDecorator = arg0.value;
-			}
-			else {
-				throw {
-					// TODO: specify file
-					message: "The 'extend' you are trying to make needs to have a string as a first parameter. Example: '...extend(\"a.b.C\", {...overrides...})'",
-					errCode: 1
-				}
-			}
-
-			var isCorrectClassName = /^(((\w+\.)+\w+)|(\w+))$/.test(classNameFromDecorator);
-			if(!isCorrectClassName) {
-				throw {
-					// TODO: specify file
-					message: "The first argument '" + classNameFromDecorator + "' of the 'extend' function is not following the right pattern which is: '[namespace.]ClassName'. Example: '...extend(\"a.b.ClassName\", {overrides...})'",
-					errCode: 1
-				}
-			}
-
-			var arg1 = path.parent.arguments[1];
-
-			if(t.isObjectExpression(arg1)) {
-				var objectProperties = arg1.properties;
-				for(var index in objectProperties) {
-					overriddenMethodNames.push(objectProperties[index].key.name)
-				}
-			}
-			else {
-				throw {
-					// TODO: specify file
-					message: "The extend you are trying to make needs to have an object as a second parameter. Example: '...extend(\"a.b.C\", {...overrides...})'",
-					errCode: 1
-				}
-			}
+		//anchor is JavaProxy
+		var customDecoratorName = config.extendDecoratorName === undefined ? defaultExtendDecoratorName : config.extendDecoratorName;
+		if(t.isIdentifier(path) && path.node.name === customDecoratorName) {
+			traverseJavaProxyExtend(path, config, customDecoratorName);
 		}
 	}
 
-	decoratorVisitor.clearData = function() {
-		isExtendKeyWord = false;
-		overriddenMethodNames = [];
-		extendClass = [];
-		classNameFromDecorator = "No decorator name found";
+	decoratorVisitor.getExtendInfo = function () {
+		return linesToWrite;
 	}
-	decoratorVisitor.getDecoratorClassName = function () {
-		if(!isExtendKeyWord) {
+
+	function traverseJavaProxyExtend(path, config, customDecoratorName) {
+		if(config.logger) {
+			config.logger.info("\t+in "+customDecoratorName+" anchor");
+		}
+
+		var classNameFromDecorator = getDecoratorArgument(path, config, customDecoratorName),
+			extendClass = getArgumentFromNodeAsString(path),
+			overriddenMethodNames = getOverriddenMethods(path);
+
+		var lineToWrite = "Java File: " + classNameFromDecorator + " - Extend Class: " + extendClass + " - Overridden Methods: " + overriddenMethodNames;
+		if(config.logger) {
+			config.logger.info(lineToWrite)
+		}
+		linesToWrite.push(lineToWrite)
+	}
+
+	function traverseNormalExtend(path, config) {
+		if(config.logger) {
+			config.logger.info("\t+in extend anchor");
+		}
+
+		var overriddenMethodNames = [],
+			extendClass = [],
+			classNameFromDecorator = "No decorator name found";
+
+		var callee = path.parent.callee;
+		if(!callee) {
 			throw {
 				// TODO: specify file
-				message: "There is no 'extend' keyword used in this file! You need to extend a native class! Example: 'some.android.Class.extend(\"my.java.Class\", {overrides...})'",
+				message: "You need to specify a name of the 'extend'. Example: '...extend(\"a.b.C\", {...overrides...})')",
 				errCode: 1
 			}
 		}
-		return classNameFromDecorator;
+
+		var o = callee.object
+		if(o) {
+			while (true) {
+				if (t.isIdentifier(o)) {
+					extendClass.push(o.name)
+					break;
+				}
+
+				extendClass.push(o.property.name)
+				o = o.object
+			}
+		}
+
+		var arg0 = path.parent.arguments[0];
+		if (t.isStringLiteral(arg0)) {
+			classNameFromDecorator = arg0.value;
+		}
+		else {
+			throw {
+				// TODO: specify file
+				message: "The 'extend' you are trying to make needs to have a string as a first parameter. Example: '...extend(\"a.b.C\", {...overrides...})'",
+				errCode: 1
+			}
+		}
+
+		var isCorrectClassName = testJavaProxyName(classNameFromDecorator);
+		if(!isCorrectClassName) {
+			throw {
+				// TODO: specify file
+				message: "The first argument '" + classNameFromDecorator + "' of the 'extend' function is not following the right pattern which is: 'namespace.[(namespace.)]ClassName'. Example: '...extend(\"a.b.ClassName\", {overrides...})'",
+				errCode: 1
+			}
+		}
+
+		var arg1 = path.parent.arguments[1];
+
+		if(t.isObjectExpression(arg1)) {
+			var objectProperties = arg1.properties;
+			for(var index in objectProperties) {
+				overriddenMethodNames.push(objectProperties[index].key.name)
+			}
+		}
+		else {
+			throw {
+				// TODO: specify file
+				message: "The extend you are trying to make needs to have an object as a second parameter. Example: '...extend(\"a.b.C\", {...overrides...})'",
+				errCode: 1
+			}
+		}
+
+		var lineToWrite = "Java File: " + classNameFromDecorator + " - Extend Class: " + extendClass.reverse().join(".") + " - Overridden Methods: " + overriddenMethodNames;
+		if(config.logger) {
+			config.logger.info(lineToWrite)
+		}
+		linesToWrite.push(lineToWrite)
 	}
-	decoratorVisitor.getExtendClass = function () {
-		return isExtendKeyWord ? extendClass.reverse().join(".") : [];
+
+	function getArgumentFromNodeAsString(path) {
+
+		var extClassArr = [];
+		var extendedClass =  getParrent(path, 8);
+
+		if(extendedClass) {
+			if(t.isCallExpression(extendedClass.node)) {
+				var o = extendedClass.node.arguments[0];	
+			}
+			else {
+				throw {
+					errCode: 1,
+					message: "Node type is not a call expression."
+				}
+			}
+		}
+
+		while(o !== undefined) {
+			
+			if(!t.isMemberExpression(o)) {
+				extClassArr.push(o.name)
+				break;
+			}
+			
+			extClassArr.push(o.property.name) //these are member expressions
+			o = o.object;
+		}
+
+		return extClassArr.reverse().join(".");
 	}
-	decoratorVisitor.getMethodNames = function () {
-		return isExtendKeyWord ? overriddenMethodNames : [];
+
+	function getDecoratorArgument(path, config, customDecoratorName) {
+		if(path.parent && t.isCallExpression(path.parent)) {
+
+			if(path.parent.arguments && path.parent.arguments.length > 0) {
+
+				var classNameFromDecorator = path.parent.arguments[0].value
+				var isCorrectClassName = testJavaProxyName(classNameFromDecorator);
+				if(isCorrectClassName) {
+					return path.parent.arguments[0].value;
+				}
+				else {
+					throw {
+						message: "The first argument '" + classNameFromDecorator + "' of the "+customDecoratorName+" decorator is not following the right pattern which is: '[namespace.]ClassName'. Example: '"+customDecoratorName+"(\"a.b.ClassName\", {overrides...})'",
+						errCode: 1
+					}
+				}
+			}
+			else {
+				throw {
+					message: "No arguments passed to "+customDecoratorName+" decorator. Example: '"+customDecoratorName+"(\"a.b.ClassName\", {overrides...})'",
+					errCode: 1
+				}
+			}
+		}
+		else {
+			throw { 
+				message: "Decorator "+customDecoratorName+" must be called with parameters: Example: '"+customDecoratorName+"(\"a.b.ClassName\", {overrides...})'",
+				errCode: 1
+			}
+		}
+		return undefined;
+	}
+
+	function getOverriddenMethods(path) {
+		var methods = [];
+
+		var cn = getParrent(path, 6)
+
+		// this pattern follows typescript generated syntax
+		for(var item in cn.node.body) {
+			var ci = cn.node.body[item];
+			if(t.isExpressionStatement(ci)) {
+				if(t.isAssignmentExpression(ci.expression)) {
+					if(ci.expression.left.property) {
+						methods.push(ci.expression.left.property.name)
+					}
+				}
+			}
+		}
+
+		return methods;
+	}
+
+	function getParrent(node, numberOfParrents) {
+		if(!node) {
+			throw "No parent found for node";
+		}
+		if(numberOfParrents === 0) {
+			return node;
+		}
+
+		return getParrent(node.parentPath, --numberOfParrents)
+	}
+
+	function testJavaProxyName(name) {
+		return /^((\w+\.)+\w+)$/.test(name)
 	}
 
 	return {
